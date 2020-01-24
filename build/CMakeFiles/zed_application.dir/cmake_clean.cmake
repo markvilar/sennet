1b2c5c45366433a848d6ea1c6c8c632a599398c3
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-01-24 14:09:31 +0100

    Added ZED and Clang to CMake.

---
+++ build/CMakeFiles/zed_application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zed_application.dir/src/main.cpp.o"
+  "zed_application"
+  "zed_application.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zed_application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
