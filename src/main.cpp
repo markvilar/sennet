ca042553282aca3e36cf99b19d509a1e3fe9ba70
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-01-24 15:58:54 +0100

    Initialized camera and added build folder to gitignore.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include <sl/Camera.hpp>
+
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	// Initialize camera
+	sl::Camera zed;
+
+	// Configuration parameters
+	sl::InitParameters init_params;
+	init_params.sdk_verbose = true;
+
+	// Open camera
+	sl::ERROR_CODE err = zed.open(init_params);
+	if (err != sl::SUCCESS)
+	{
+		std::cout << "Couldn't open ZED camera..." << std::endl;
+		exit(-1);
+	}
+
+	// Close camera
+	zed.close();
+
+	std::cout << "ZED application executed!" << std::endl;
 	return 0;
 }
