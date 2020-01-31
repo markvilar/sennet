e79889bcc1fb125d202ddd783a80eff2ac8d56bd
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-01-31 15:32:11 +0100

    Added various console print functions.

--- include/zedutils/ZEDio.hpp
+++ include/zedutils/ZEDio.hpp
@@ -3,5 +3,12 @@
 #include <iostream>
 #include <string>
 
-void print(std::string msg_prefix, sl::ERROR_CODE err_code, 
+
+void print(std::string msg_prefix, const sl::ERROR_CODE err_code, 
 		std::string msg_suffix);
+
+
+std::ostream& operator<<(std::ostream& os, const sl::InitParameters& params);
+std::ostream& operator<<(std::ostream& os, const sl::RuntimeParameters& params);
+std::ostream& operator<<(std::ostream& os, const sl::CameraInformation& params);
+std::ostream& operator<<(std::ostream& os, const sl::Resolution& res);
--- src/ZEDio.cpp
+++ src/ZEDio.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-void print(std::string msg_prefix, sl::ERROR_CODE err_code, 
+void print(std::string msg_prefix, const sl::ERROR_CODE err_code, 
 		std::string msg_suffix)
 {
 	std::cout << "[Sample]";
@@ -30,4 +30,70 @@ void print(std::string msg_prefix, sl::ERROR_CODE err_code,
 	}
 
 	std::cout << std::endl;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const sl::InitParameters& params)
+{
+	os << "\nInitParameters:\n";
+	os << " - Resolution:              " << params.camera_resolution << "\n";
+	os << " - Fps:                     " << params.camera_fps << "\n";
+	os << " - Image flip:              " << params.camera_image_flip << "\n";
+	os << " - Svo real time mode:      " 
+		<< params.svo_real_time_mode << "\n";
+	os << " - Depth mode:              " 
+		<< params.depth_mode << "\n";
+	os << " - Depth stabilization:     " 
+		<< params.depth_stabilization << "\n";
+	os << " - Depth minimum dist.:     " 
+		<< params.depth_minimum_distance << "\n";
+	os << " - Depth maximum dist.:     " 
+		<< params.depth_maximum_distance << "\n";
+	os << " - Unit:                    " << params.coordinate_units << "\n";
+	os << " - Coordinate system:       " << params.coordinate_system << "\n";
+	os << " - SDK gpu id:              " << params.sdk_gpu_id << "\n";
+	os << " - SDK verbose:             " << params.sdk_verbose << "\n";
+	os << " - Sensors required:        " << params.sensors_required << "\n";
+	os << " - Enable image enh.:       " 
+		<< params.enable_image_enhancement << "\n";
+	return os;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const sl::RuntimeParameters& params)
+{
+	os << "\nRuntimeParameters:\n";
+	os << " - Sensing mode:            " << params.sensing_mode << "\n";
+	os << " - 3D reference frame:      " 
+		<< params.measure3D_reference_frame << "\n";
+	os << " - Enable depth:            " << params.enable_depth << "\n";
+	os << " - Conf. thresh.:           " 
+		<< params.confidence_threshold << "\n";
+	os << " - Texture conf. thresh.:   " 
+		<< params.textureness_confidence_threshold << "\n";
+	return os;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const sl::CameraInformation& params)
+{
+	os << "\nCameraInformation:\n";
+	os << " - Serial number:           " << params.serial_number << "\n";
+	os << " - Camera firmware version: " 
+		<< params.camera_firmware_version << "\n";
+	os << " - Sensor firmware version: " 
+		<< params.sensors_firmware_version << "\n";
+	os << " - Camera model:            " << params.camera_model << "\n";
+	os << " - Input type:              " 
+		<< params.input_type << "\n";
+	os << " - Fps:                     " << params.camera_fps << "\n";
+	os << " - Resolution:              " << params.camera_resolution << "\n";
+	return os;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const sl::Resolution& res)
+{
+	os << res.width << "x" << res.height << "\n";
+	return os;
 }
