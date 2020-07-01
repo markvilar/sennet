5e35aaf1fbf49c6e90f6bbe711fe4ecc5d4b12ed
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-07-01 15:54:29 +0200

    Add static log and remove handler constructor.

--- include/sennet/log.hpp
+++ include/sennet/log.hpp
@@ -4,31 +4,51 @@
 
 #include <spdlog/spdlog.h>
 
-#include <sennet/am/core.hpp>
-
 namespace sennet 
 {
 
 class log
 {
 public: 
-	// Console logger constructor.
-	log(const std::string& name);
+	static void init();
 
-	// Console and file logger constructor.
-	log(const std::string& name, const std::string& file_path);
-
-	// Destructor.
-	~log();
-
-	// Returns the underlying spdlog logger.
-	inline std::shared_ptr<spdlog::logger>& get_logger() 
-	{ 
-		return m_logger;
+	static std::shared_ptr<spdlog::logger>& get_core_logger() 
+	{
+		return s_core_logger;
+	}
+	
+	static std::shared_ptr<spdlog::logger>& get_client_logger() 
+	{
+		return s_client_logger;
 	}
 
 private:
-	std::shared_ptr<spdlog::logger> m_logger;
+	static std::shared_ptr<spdlog::logger> s_core_logger;
+	static std::shared_ptr<spdlog::logger> s_client_logger;
 };
 
 };
+
+// Core log macros.
+#define SN_CORE_TRACE(...) \
+	::sennet::log::get_core_logger()->trace(__VA_ARGS__)
+#define SN_CORE_INFO(...) \
+	::sennet::log::get_core_logger()->info(__VA_ARGS__)
+#define SN_CORE_WARN(...) \
+	::sennet::log::get_core_logger()->warn(__VA_ARGS__)
+#define SN_CORE_ERROR(...) \
+	::sennet::log::get_core_logger()->error(__VA_ARGS__)
+#define SN_CORE_CRITICAL(...) \
+	::sennet::log::get_core_logger()->critical(__VA_ARGS__)
+
+// Client log macros.
+#define SN_TRACE(...) \
+	::sennet::log::get_client_logger()->trace(__VA_ARGS__)
+#define SN_INFO(...) \
+	::sennet::log::get_client_logger()->info(__VA_ARGS__)
+#define SN_WARN(...) \
+	::sennet::log::get_client_logger()->warn(__VA_ARGS__)
+#define SN_ERROR(...) \
+	::sennet::log::get_client_logger()->error(__VA_ARGS__)
+#define SN_CRITICAL(...) \
+	::sennet::log::get_client_logger()->critical(__VA_ARGS__)
--- src/log.cpp
+++ src/log.cpp
@@ -6,39 +6,33 @@
 namespace sennet 
 {
 
-log::log(const std::string& name)
+std::shared_ptr<spdlog::logger> log::s_core_logger;
+std::shared_ptr<spdlog::logger> log::s_client_logger;
+
+void log::init()
 {
-	// Create console sink.
-	auto console_sink = 
-		std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-	console_sink->set_level(spdlog::level::trace);
-	console_sink->set_pattern("%^[%T] %n: %v%$");
+	std::vector<spdlog::sink_ptr> sinks;
+	sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+		"sennet.log", true));
+		
 
-	// Create logger.
-	m_logger.reset(new spdlog::logger(name, console_sink));
-	m_logger->set_level(spdlog::level::trace);
-}
+	sinks[0]->set_pattern("%^[%T] %n: %v%$");
+	sinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-log::log(const std::string& name, const std::string& file_path)
-{
-	// Create console sink.
-	auto console_sink = 
-		std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-	console_sink->set_level(spdlog::level::trace);
-	console_sink->set_pattern("%^[%T] %n: %v%$");
+	// Core logger.
+	s_core_logger = std::make_shared<spdlog::logger>(
+		"SENNET", begin(sinks), end(sinks));
+	spdlog::register_logger(s_core_logger);
+	s_core_logger->set_level(spdlog::level::trace);
+	s_core_logger->flush_on(spdlog::level::trace);
 
-	// Create file sink.
-	auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-		file_path, true);
-	file_sink->set_level(spdlog::level::trace);
-
-	// Create logger.
-	m_logger.reset(new spdlog::logger(name, {console_sink, file_sink}));
-	m_logger->set_level(spdlog::level::trace);
-}
-
-log::~log()
-{
+	// Client logger.
+	s_client_logger = std::make_shared<spdlog::logger>(
+		"APP", begin(sinks), end(sinks));
+	spdlog::register_logger(s_client_logger);
+	s_client_logger->set_level(spdlog::level::trace);
+	s_client_logger->flush_on(spdlog::level::trace);
 }
 
 };
