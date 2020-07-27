36fc8366cbf266220ed5922f38faf96a6c768c2a
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-07-27 22:33:27 +0200

    Add glm as submodule and test input polling in sandbox.

--- apps/sandbox.cpp
+++ apps/sandbox.cpp
@@ -1,5 +1,22 @@
 #include <sennet/sennet.hpp>
 
+#include <glm/glm.hpp>
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+
+glm::mat4 camera(float Translate, glm::vec2 const & Rotate)
+{
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
+	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+	return Projection * View * Model;
+} 
+
 class example_layer : public sennet::layer
 {
 public:
@@ -10,12 +27,24 @@ public:
 
 	void on_update() override
 	{
-		SN_INFO("example_layer::on_update");
+		if (sennet::input::is_key_pressed(SN_KEY_TAB))
+			SN_TRACE("Tab key is pressed (poll)!");
 	}
 
 	void on_event(sennet::event& event) override
 	{
-		SN_TRACE("{0}", event.to_string());
+		if (event.get_event_type() == sennet::event_type::key_pressed)
+		{
+			sennet::key_pressed_event& e =
+				(sennet::key_pressed_event&)event;
+			
+			if (e.get_key_code() == SN_KEY_TAB)
+			{
+				SN_TRACE("Tab key is pressed (event)!");
+			}
+
+			SN_TRACE("{0}", (char)e.get_key_code());
+		}
 	}
 };
 
