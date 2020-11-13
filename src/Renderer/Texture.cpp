404c75132da2bf427838de7a0088eb9199e859fd
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-11-13 18:02:48 +0100

    Add internal- and data format specification to Texture class.

--- src/Renderer/Texture.cpp
+++ src/Renderer/Texture.cpp
@@ -8,7 +8,7 @@
 namespace Sennet
 {
 
-Ref<Texture2D> Texture2D::Create(const uint32_t& width, const uint32_t& height)
+Ref<Texture2D> Texture2D::Create(const uint32_t width, const uint32_t height)
 {
 	switch (Renderer::GetAPI())
 	{
@@ -16,7 +16,25 @@ Ref<Texture2D> Texture2D::Create(const uint32_t& width, const uint32_t& height)
 			SN_CORE_ASSERT(false, "Renderer API None is currently \
 				not supported!");
 		case RendererAPI::API::OpenGL:
-			return CreateRef<OpenGLTexture2D>(width, height);
+			return CreateRef<OpenGLTexture2D>(width, height,
+				InternalFormat::RGBA8, DataFormat::RGBA);
+	}
+	
+	SN_CORE_ASSERT(false, "Unknown Renderer API.");
+	return nullptr;
+}
+
+Ref<Texture2D> Texture2D::Create(const uint32_t width, const uint32_t height,
+	const InternalFormat internalFormat, const DataFormat dataFormat)
+{
+	switch (Renderer::GetAPI())
+	{
+		case RendererAPI::API::None: 
+			SN_CORE_ASSERT(false, "Renderer API None is currently \
+				not supported!");
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height,
+				internalFormat, dataFormat);
 	}
 	
 	SN_CORE_ASSERT(false, "Unknown Renderer API.");
